973cf255ef236d25630509a899857230618eb9ce
Liam Gray <lgray@phas.ubc.ca>
2026-04-16 11:17:10 -0700

    feat(ringbuffer): add some tests and fix bugs

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -33,35 +33,38 @@ pub async fn data(State(state): State<SharedDataState>) -> impl IntoResponse {
 
     // Dump all the current buffers
     if let Some(frac_flagged) = state.frac_flagged.get() {
+        let len = frac_flagged.len();
         let frac_flagged = frac_flagged
             .serialize()
             .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
         result.insert(
             "frac_flagged".into(),
-            json!({"frame_count": frac_flagged.len(), "frames": frac_flagged}),
+            json!({"frame_count": len, "frames": frac_flagged}),
         );
     }
 
     if let Some(sktilde_avg) = state.sktilde_avg.get() {
+        let len = sktilde_avg.len();
         let sktilde_avg = sktilde_avg
             .serialize()
             .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
         result.insert(
             "sktilde_avg".into(),
-            json!({"frame_count": sktilde_avg.len(), "frames": sktilde_avg}),
+            json!({"frame_count": len, "frames": sktilde_avg}),
         );
     }
 
     if let Some(bad_feed_counts) = state.bad_feed_counts.get() {
+        let len = bad_feed_counts.len();
         let bad_feed_counts = bad_feed_counts
             .serialize()
             .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
         result.insert(
             "bad_feed_counts".into(),
-            json!({"frame_count": bad_feed_counts.len(), "frames": bad_feed_counts}),
+            json!({"frame_count": len, "frames": bad_feed_counts}),
         );
     }
 
